db34764e9e83202b3b09f9aa530f901bda3a393c
Grimaldo <gqpsystem@gmail.com>
2018-09-04 16:32:36 -0500

    gqpsystem@gmail.com

---
+++ src/main/java/com/megacenter/MegasisApplication.java
@@ -0,0 +1,12 @@
+package com.megacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegasisApplication.class, args);
+	}
+}
